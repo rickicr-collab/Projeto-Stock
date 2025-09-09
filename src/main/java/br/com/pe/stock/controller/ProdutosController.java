754afd1f4e8d183cd7c7f0efec2535bf92a0312e
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-09-09 14:36:00 -0300

    configured thymeleaf and viewResolver of the controller

--- src/main/java/br/com/pe/stock/controller/ProdutosController.java
+++ src/main/java/br/com/pe/stock/controller/ProdutosController.java
@@ -1,5 +1,14 @@
 package br.com.pe.stock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ProdutosController {
+	
+	@RequestMapping("/produtos/novo")
+	public String novo() {
+		return "produto/CadastroProduto";
+	}
 
 }
